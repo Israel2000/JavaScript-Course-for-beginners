6481a044fbc40a68ffb6d7d15d8a0d10530e298b
Israel Belete <ibelete2000@gmail.com>
2020-06-23 22:41:40 -0600

    added new condition file

---
+++ conditions.js
@@ -0,0 +1,13 @@
+// this page belong to conditional js only
+
+if (1 > 0) {
+    console.log("yes, I knew it.");
+}
+
+if (0 > 1) {
+    console.log("not true");
+} else {
+    console.log("you are correct");
+}
+
+// testing more than one case
