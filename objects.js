6fe4e4138bd8a5dcac19283274c756193377643e
Israel Belete <ibelete2000@gmail.com>
2020-07-10 21:29:11 -0600

    added objects.js

---
+++ objects.js
@@ -0,0 +1,24 @@
+/* In this lesson we will identify objects, attributes and methods in real scenarios
+- create a constructor for JS object
+- write prototype method for js object
+*/
+// Two main way creating objects:
+// 
+let myFavCities = {};
+
+let myCar = new Object();
+
+let myMotorcycle = {
+    wheels: 2,
+    color: "lakers gold",
+    maxSpeed: 250,
+    owners: ["Mr.Bean", "Jean"]
+}
+
+// we can get and set object prop w/ dot notation(.) or square brackets[]
+myFavCities.losAngeles = 4;
+myFavCities.ocean = "venice Beach, Santa Monica";
+
+// []
+myCar["num-of-wheels"];
+
